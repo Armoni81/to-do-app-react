5d811fa68ecfe9c94b362edf25a13af308ca11cf
Armoni Tigner <atigner2@student.gsu.edu>
2024-07-25 21:38:31 -0400

    fixed space

--- src/Components/allTodoFunctionality/removeAll.js
+++ src/Components/allTodoFunctionality/removeAll.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const RemoveAllTodo = ({ setCheckedStatus, checkedStatus }) => {
+    
     return( 
         <div>
             <button onClick={checkedStatus.length >= 2 ? () => setCheckedStatus([]) : null}>Remove All</button>
